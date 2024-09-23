744e814a1ed16560194728ce127dc7a6c7dc7db0
sonalichaudhary844@gmail.com <sonalichaudhary884>
2024-09-23 23:35:44 +0530

    add new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player1" symbol="X" />
+          <Player initialName="Player2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
