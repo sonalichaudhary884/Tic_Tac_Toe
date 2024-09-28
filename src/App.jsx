dd2147faf0a19b44e8c592e17324bbca1ce9696e
Ravi Chaudhary <ravichaudhary@lambdatest.com>
2024-09-29 00:12:31 +0530

    fixed app component

--- src/App.jsx
+++ src/App.jsx
@@ -38,70 +38,93 @@ function deriveGameBoard(gameTurns) {
   return gameBoard;
 }
 
-const [players, setPlayers] = useState(PLAYERS);
+function deriveWinner(gameBoard, players) {
+  let winner;
 
-const [gameTurns, setGameTurns] = useState([]);
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
 
-const activePlayer = deriveActivePlayer(gameTurns);
-const gameBoard = deriveGameBoard(gameTurns);
-const winner = deriveWinner(gameBoard, players);
-const hasDraw = gameTurns.length === 9 && !winner;
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
 
-function handleSelectSquare(rowIndex, colIndex) {
-  // setActivePlayer((curActive) => (curActive === "X" ? "O" : "X"));
-  setGameTurns((prevTurns) => {
-    const currentPlayer = deriveActivePlayer(prevTurns);
-
-    const updatedTurns = [
-      { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
-      ...prevTurns,
-    ];
-
-    return updatedTurns;
-  });
+  return winner;
 }
 
-function handleRestart() {
-  setGameTurns([]);
-}
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
+  const hasDraw = gameTurns.length === 9 && !winner;
 
-function handlePlayerNameChange(symbol, newName) {
-  setPlayers((prevPlayers) => {
-    return {
-      ...prevPlayers,
-      [symbol]: newName,
-    };
-  });
-}
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActive) => (curActive === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-return (
-  <main>
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player
-          initialName={PLAYERS.X}
-          symbol="X"
-          isActive={activePlayer === "X"}
-          onChangeName={handlePlayerNameChange}
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName={PLAYERS.X}
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName={PLAYERS.O}
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
+        </ol>
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          board={gameBoard}
+          turns={gameTurns}
         />
-        <Player
-          initialName={PLAYERS.O}
-          symbol="O"
-          isActive={activePlayer === "O"}
-          onChangeName={handlePlayerNameChange}
-        />
-      </ol>
-      {(winner || hasDraw) && (
-        <GameOver winner={winner} onRestart={handleRestart} />
-      )}
-      <GameBoard
-        onSelectSquare={handleSelectSquare}
-        board={gameBoard}
-        turns={gameTurns}
-      />
-    </div>
-    <Log turns={gameTurns} />
-  </main>
-);
+      </div>
+      <Log turns={gameTurns} />
+    </main>
+  );
+}
 
 export default App;
